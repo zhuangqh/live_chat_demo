f6d94c7649cbc7f11cce6ab62fffd8cb38caf221
laiy <ly.franky@gmail.com>
2016-02-25 11:51:20 +0800

    server multi thread done

--- concurrent_multi_thread_block_IO/server.c
+++ concurrent_multi_thread_block_IO/server.c
@@ -8,16 +8,19 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
+#include <pthread.h>
 #include "awesome_handler.h"
 
 #define LISTENQ 5
+#define MAX_CONNECTIONS 1024
+
+int client[MAX_CONNECTIONS];
+static void *boardcast(void *);
 
 int main() {
-    int i, j, maxi, maxfd, listenfd, connfd, sockfd, tmpfd;
-    int nready, client[FD_SETSIZE];
-    ssize_t n;
-    fd_set rset, allset;
-    char buf[MAXLINE];
+    int listenfd, i;
+    long connfd;
+    pthread_t tid;
     socklen_t clilen;
     struct sockaddr_in client_address, server_address;
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,53 +30,36 @@ int main() {
     server_address.sin_port = htons(SERVER_PORT);
     bind(listenfd, (struct sockaddr *)&server_address, sizeof(server_address));
     listen(listenfd, LISTENQ);
-    maxfd = listenfd;
-    maxi = -1;
-    for (i = 0; i < FD_SETSIZE; i++)
+    for (i = 0; i < MAX_CONNECTIONS; i++)
         client[i] = -1;
-    FD_ZERO(&allset);
-    FD_SET(listenfd, &allset);
     for (;;) {
-        rset = allset;
-        nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
-        if (FD_ISSET(listenfd, &rset)) {
-            clilen = sizeof(client_address);
-            connfd = accept(listenfd, (struct sockaddr *)&client_address, &clilen);
-            for (i = 0; i < FD_SETSIZE; i++)
-                if (client[i] < 0) {
-                    client[i] = connfd;
-                    break;
-                }
-            if (i == FD_SETSIZE)
-                error("too many clients");
-            FD_SET(connfd, &allset);
-            if (connfd > maxfd)
-                maxfd = connfd;
-            if (i > maxi)
-                maxi = i;
-            if (--nready <= 0)
-                continue;
-        }
-        for (i = 0; i <= maxi; i++) {
-            if ((sockfd = client[i]) < 0)
-                continue;
-            if (FD_ISSET(sockfd, &rset)) {
-                if ((n = read(sockfd, buf, MAXLINE)) == 0) {
-                    close(sockfd);
-                    FD_CLR(sockfd, &allset);
-                    client[i] = -1;
-                } else {
-                    for (j = 0; j <= maxi; j++) {
-                        if ((tmpfd = client[j]) < 0 || sockfd == tmpfd)
-                            continue;
-                        write(tmpfd, buf, n);
-                    }
-                }
-                if (--nready <= 0)
-                    break;
+        clilen = sizeof(client_address);
+        connfd = accept(listenfd, (struct sockaddr *)&client_address, &clilen);
+        for (i = 0; i < MAX_CONNECTIONS; i++)
+            if (client[i] < 0) {
+                client[i] = connfd;
+                break;
             }
-        }
+        if (i == MAX_CONNECTIONS)
+            error("too many clients");
+        pthread_create(&tid, NULL, &boardcast, (void *)connfd);
     }
     return 0;
+}
+
+static void *boardcast(void *arg) {
+    ssize_t n;
+    char buf[MAXLINE];
+    int j, tmpfd;
+    pthread_detach(pthread_self());
+    while ((n = read((long)arg, buf, MAXLINE)) > 0) {
+        for (j = 0; j < MAX_CONNECTIONS; j++) {
+            if ((tmpfd = client[j]) < 0 || (long)arg == tmpfd)
+                continue;
+            write(tmpfd, buf, n);
+        }
+    }
+    close((long)arg);
+    return NULL;
 }
 
