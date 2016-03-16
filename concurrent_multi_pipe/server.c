efbadc64ab89504708e771a643329207972d9376
zhuangqh <zhuangqhc@163.com>
2016-03-15 22:34:06 -0700

    concurrent using pipe

--- concurrent_multi_pipe/server.c
+++ concurrent_multi_pipe/server.c
@@ -12,89 +12,123 @@
 #include "awesome_handler.h"
 
 #define LISTENQ 5
-#define MAX_CONNECTIONS 1024
 
-int client[MAX_CONNECTIONS];
-int pipe_fd[MAX_CONNECTIONS][2];
-static void boardcast(long, int);
+
+int client[FD_SETSIZE];
+int pipe_fd[FD_SETSIZE][2];
+static void to_boardcast(long, int);
+static void *Listen(void *arg);
+pid_t pid;
+int maxfd, maxi;
+fd_set allset, rset;
+int start;
 
 int main() {
-  int listenfd, i, j;
-    long connfd;
-    pid_t pid;
-    socklen_t clilen;
-    struct sockaddr_in client_address, server_address;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&server_address, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(SERVER_PORT);
-    bind(listenfd, (struct sockaddr *)&server_address, sizeof(server_address));
-    listen(listenfd, LISTENQ);
-    for (i = 0; i < MAX_CONNECTIONS; i++)
-      client[i] = pipe_fd[i][0] = pipe_fd[i][1] = -1;
-    for (;;) {
-        clilen = sizeof(client_address);
-        connfd = accept(listenfd, (struct sockaddr *)&client_address, &clilen);
-        for (i = 0; i < MAX_CONNECTIONS; i++)
-            if (client[i] < 0) {
-                client[i] = connfd;
-                break;
-            }
-        if (i == MAX_CONNECTIONS)
-            error("too many clients");
-        printf("connfd %ld\n", connfd);
+  int i, nready, sockfd, j, n, tmpfd;
+  char buf[MAXLINE];
+  pthread_t tid;
+  // thread to listen message send from client
+  pthread_create(&tid, NULL, &Listen, NULL);
 
-        // create pipe
-        if (pipe(pipe_fd[i]) == 0) {
+  for (i = 0; i < FD_SETSIZE; ++i)
+    pipe_fd[i][0] = pipe_fd[i][1] = -1;
+  FD_ZERO(&allset);
+  maxfd = -1;
+  maxi = -1;
+  pid = -1;
+  start = 0;
+  for (;;) {
+    if (pid == 0) break;
+    if (start) {
+      rset = allset;
+      printf("befor selece\n");
+      nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+      for (i = 0; i <= maxi; ++i) {
+        if ((sockfd = pipe_fd[i][0]) < 0)
+          continue;
+        if (FD_ISSET(sockfd, &rset)) {
+          printf("start handle\n");
+          if ((n = read(sockfd, buf, MAXLINE)) == 0) {
+            close(sockfd);
+            FD_CLR(sockfd, &allset);
+            client[i] = -1;
+          } else {
+            for (j = 0; j <= maxi; ++j) {
+              if ((tmpfd = client[i]) < 0)
+                continue;
 
-          if ((pid = fork()) == -1)
-            error("fork failed\n");
-          if (pid == 0) {
-            printf("child process begin\n");
-            close(listenfd);
-            boardcast(connfd, pipe_fd[i][0]);
-            exit(0);
-          }
-
-          // update data
-          for (j = 0; j < MAX_CONNECTIONS; j++) {
-            if (pipe_fd[j][0] != -1) {
-              write(pipe_fd[j][1], client, sizeof(int) * MAX_CONNECTIONS);
-              printf("update in %d\n", j);
+              write(tmpfd, buf, n);
             }
           }
-          printf("close child process's connfd in father process\n");
-          close(connfd);
-        } else {
-          error("create pipe error");
         }
+      }
     }
-    return 0;
+  }
+  return 0;
 }
 
-static void boardcast(long connfd, int fd) {
-  int true_client[MAX_CONNECTIONS];
+static void *Listen(void *arg) {
+  pthread_detach(pthread_self());
+  int listenfd, i;
+  long connfd;
+  socklen_t clilen;
+  printf("hello");
+  struct sockaddr_in client_address, server_address;
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&server_address, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_address.sin_port = htons(SERVER_PORT);
+  bind(listenfd, (struct sockaddr *)&server_address, sizeof(server_address));
+  listen(listenfd, LISTENQ);
+  for (i = 0; i < FD_SETSIZE; i++)
+    client[i] = pipe_fd[i][0] = pipe_fd[i][1] = -1;
+
+  for (;;) {
+    clilen = sizeof(client_address);
+    connfd = accept(listenfd, (struct sockaddr *)&client_address, &clilen);
+    for (i = 0; i < FD_SETSIZE; i++)
+      if (client[i] < 0) {
+        client[i] = connfd;
+        break;
+      }
+    if (i == FD_SETSIZE)
+      error("too many clients");
+    if (i > maxi) maxi = i;
+    if (!start) start = 1;
+
+    // create pipe
+    if (pipe(pipe_fd[i]) != 0) {
+      error("create pipe error");
+    } else {
+
+      if ((pid = fork()) == -1)
+        error("fork failure\n");
+      if (pid == 0) {
+        printf("child process begin\n");
+        close(listenfd);
+        to_boardcast(connfd, pipe_fd[i][1]);
+        exit(0);
+      }
+
+      close(connfd);
+      FD_SET(pipe_fd[i][1], &allset);
+      if (pipe_fd[i][0] > maxfd)
+        maxfd = pipe_fd[i][1];
+    }
+  }
+  return NULL;
+}
+
+static void to_boardcast(long connfd, int pipefd) {
   ssize_t n;
   char buf[MAXLINE];
-  int j, tmpfd;
 
   while ((n = read(connfd, buf, MAXLINE)) > 0) {
-    // read data from pipe
-    read(fd, true_client, sizeof(int) * MAX_CONNECTIONS);
     printf("connfd %ld ", connfd);
-    for (int i = 0; i < 3; ++i) {
-      printf("%d ", true_client[i]);
-    }
-    printf("\n");
-    close(fd);
     printf("enter boardcast\n");
-    for (j = 0; j < MAX_CONNECTIONS; j++) {
-      if ((tmpfd = true_client[j]) < 0 || connfd == tmpfd)
-        continue;
-      printf("cast\n");
-      write(tmpfd, buf, n);
-    }
+    write(pipefd, buf, strlen(buf));
+    close(pipefd);
   }
   printf("end of boardcast\n");
   close(connfd);
